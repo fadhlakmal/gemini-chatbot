384f35163c57c3ae9cc18db9fc95317a4b8245e4
fadhlakmal <akmal.madany1@gmail.com>
2024-01-12 22:42:05 +0700

    config env

--- src/lib/firebaseConfig.js
+++ src/lib/firebaseConfig.js
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyAsSTJ-xI7AXfDtC-RLKQ4k7LvdxCkRwyE",
-    authDomain: "sveltekit-chatbot-3737e.firebaseapp.com",
-    projectId: "sveltekit-chatbot-3737e",
-    storageBucket: "sveltekit-chatbot-3737e.appspot.com",
-    messagingSenderId: "233362599926",
-    appId: "1:233362599926:web:1a8ac10a6c96215e3161d7"
-};
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_APP_ID,
+  };
+  
